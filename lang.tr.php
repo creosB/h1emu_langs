ec7b25beb371720fe4919f751b1c687980a06739
creosB <baris0631@hotmail.com>
2023-02-01 20:33:52 +0300

    Turkish Language file
    This file has been updated and translated in accordance with the text and rules found in the main file.

---
+++ lang.tr.php
@@ -0,0 +1,314 @@
+<?php
+/* 
+------------------
+Language: Turkish
+------------------
+*/
+
+//$lang = array();
+
+$lang = [
+	/* ******************************************************************
+		LANGS
+	** ***************************************************************** */
+	'GOTO'	=> "Git",
+	
+	'LANG_CHOOSE'	=> "Diller",
+	'FR'		=> "Fransızca",
+	'US'		=> "İngilizce",
+	'PT'		=> "Portekizce",
+	'SE'		=> "İsveççe",
+	'PL'		=> "Lehçe",
+	'CN'		=> "Çince (basitleştirilmiş)",
+	'IT'		=> "İtalyanca",
+	'RU'		=> "Rusça",
+	'TR'		=> "Türkçe",
+
+
+	/* ******************************************************************
+		MAINMENU
+	** ***************************************************************** */
+	'MM_HOME'		=> "Anasayfa",
+	'MM_COMMUNITY'	=> "Topluluk",
+	'MM_CHANGELOG'	=> "Güncellemeler",
+	'MM_STATUS'		=> "Sunucu Listesi",
+	'MM_DOWNLOADS'	=> "İndir",
+	'MM_MEDIA'		=> "Görseller",
+	'MM_WIKI'		=> "Rehber",
+	'MM_RECIPES'		=> "Tarifler",
+
+	/* ******************************************************************
+		PAGE CONTENT & TITLES <title>
+	** ***************************************************************** */
+	'PAGE_HOME' 			=> "H1EMU'ya Hoşgeldiniz",
+	'PAGE_DOWNLOADS'		=> "İndir",
+	'PAGE_MEDIA'			=> "Görseller",
+	'PAGE_WIKI'				=> "Rehber",
+	'PAGE_CHANGELOG'		=> "Son Güncelleme",
+	'PAGE_BOARDS'			=> "Forum",
+	'PAGE_OURTEAM'			=> "Ekibimiz",
+
+	'PAGE_HOME_ABOUT'		=> "Oyun hakkında",
+
+
+	'DL_OPTCAT_1'	=> "İstemci",
+	'DL_OPTCAT_TXT_1'	=> "İstemciyi indirin ve oyunu <span class='text-h1'>oynayın</span>",
+	'DL_OPTCAT_2'	=> "Sunucu",
+	'DL_OPTCAT_TXT_2'	=> "<span class='text-h1'>Kendi</span> sunucunuzu kurun!",
+	'DL_OPTCAT_3'	=> "Ekstra",
+	'DL_OPTCAT_TXT_3'	=> "Yeni bilgiler edinin",
+	
+
+	/* ******************************************************************
+		STATUS PAGE
+	** ***************************************************************** */
+
+	'PAGE_STATUS'			=> "Sunucu Listesi",
+	'PAGE_SERVER_STATUS'	=> "Sunucu Detayları",
+	
+	'SERVER_LOGINAVAILABLE'	=> "Girişler Aktif mi?",
+	'SERVER_COQUALITY'		=> "Durum",
+	'SERVER_POP'			=> "Doluluk",
+
+	'PAGE_OFFICIALS_SERVERS'		=> "Resmi Sunucular",
+	'PAGE_COMMUNITY_SERVERS'		=> "Topluluk Sunucuları",
+
+	/* ******************************************************************
+		CONTENT BOX
+	** ***************************************************************** */
+	'CONTENT_MEDIA_INFORMATION'	=> "Tüm görüntüler oyun içerisinden kayıt edilmiştir(H1Emu). Resimlerin hepsi günceldir.",
+
+	/* ******************************************************************
+		WIKI
+	** ***************************************************************** */
+		// Nav separator
+	'MS_WIKI_SEP_MANAGE'	=> "Yönet",
+	'MS_WIKI_SEP_CATS'		=> "Kategoriler",
+
+		// Nav Buttons
+	'MS_WIKI_HOME'			=> "Rehber",
+	'MS_WIKI_CREATEPAGE'	=> "Sayfa Oluştur",
+
+
+	'TITLE_WIKI_CATEGORY' => "Kategori",
+
+	/* ******************************************************************
+		BOARDS
+	** ***************************************************************** */
+	'BOARD_STATISTICS'	=> "İstatistikler",
+	'BOARD_STAT_TOTALSUBJECTS'	=> "Toplam Konu",
+	'BOARD_STAT_TOTALMEMBER'	=> "Toplam Üye",
+	'BOARD_STAT_LASTPOST'	=> "Son Gönderi",
+
+	/* ******************************************************************
+		COCKPIT
+	** ***************************************************************** */
+	'COCKPIT_TITLE_LEGACY'	=> "Üyelik ile bağlan",
+	'COCKPIT_TITLE_THIRD'	=> "Diğer yöntemler",
+	'COCKPIT_REGISTER'	=> "Kaydol",
+	'COCKPIT_FORGOT'	=> "Şifreni mi unuttun?",
+
+
+	'COCKPIT_MYACCOUNT'	=> "Hesabım",
+	'COCKPIT_REGISTER'	=> "Kaydol",
+	'COCKPIT_LOGOUT'	=> "Çıkış yap",
+
+	// TAB "VIRTUALS CATEGORIES"
+	'COCKPIT_VC_PROFILE'	=> "Profil",
+	'COCKPIT_VC_ACCOUNT'	=> "Hesap",
+	'COCKPIT_VC_LAUNCHER'	=> "İstemci",
+	'COCKPIT_VC_SERVERS'	=> "Sunucular",
+
+	// MENU
+	'COCKPIT_HOME'			=> "Ana sayfa",
+
+	'COCKPIT_ACCOUNT_EDIT'	=> "Hesabı Düzenle",
+	'COCKPIT_PROFILE_PRIVACY'	=> "Gizlilik Ayarları",
+	'COCKPIT_CHANGE_PASSWORD'	=> "Şifreni Değiştir",
+	
+	'COCKPIT_SREQLIST'	=> "İsteklerim",
+	'COCKPIT_SREQREQUEST'	=> "İstek Gönder",
+
+	'COCKPIT_SECURITYLAUNCH'=> "Hesap Anahtarı",
+	'COCKPIT_SECURITYPASS'	=> "Şifreni Değiştir",
+	'COCKPIT_SECURITY2FA'	=> "2FA",
+	'COCKPIT_2FA'			=> "2FA Doğrulama",
+
+	'COCKPIT_LOGIN'	=> "Bağlan",
+	// 'COCKPIT_LOGIN_STEAM'	=> "Steam ile bağlan",
+
+	
+
+	// TAB ACCOUNT TITLES PAGES
+	'COCKPIT_TITLE_PAGE_HOME'		=> "Kontrol Paneli",
+
+	'COCKPIT_TITLE_PAGE_ACCOUNT_EDIT'	=> "Hesabımı düzenle",
+
+
+	'COCKPIT_TITLE_PAGE_SECURITYPASS'	=> "Şifre Değiştirme",
+	'COCKPIT_TITLE_PAGE_SECURITY2FA'	=> "2FA Güvenliği",
+	'COCKPIT_TITLE_PAGE_SECURITYLAUNCH'	=> "Hesap Anahtarı",
+	'COCKPIT_TITLE_PAGE_LAUNCH_YOURKEYIS'		=> "Anahtarınız",
+	'COCKPIT_TITLE_PAGE_LAUNCH_TEXT'	=> "Bu anahtar, istemciye giriş yapmak için kullanılır. Yardıma ihtiyacın olursa <br>bu <a class='wikilink' href=''>link</a> ile ulaşabilirsin.",
+
+	'COCKPIT_TITLE_PAGE_2FA_BACKUPCODES'		=> "Kurtarma Kodu",
+	'COCKPIT_TITLE_PAGE_2FA_BACKUPCODES_TEXT'	=> "Bu kodlar sadece acil durumlarda kullanılır.2 adımlı doğrulama ile kimliğinizi doğrulayamıyorsanız kullanın.",
+	'COCKPIT_TITLE_PAGE_2FA_BACKUPCODES_BTN'	=> "Kurtarma Kodunu üret",
+
+	'COCKPIT_ADVERT_REQUEST'	=> "İstek gönderme aslında <b>Beta sürümünde</b>.<br>Tüm işlemler <b class=''>el ile</b> kontrol ediliyor. Sunucunuz listede kullanılabilir olduğunda e-posta alacaksınız.<br><br>İstek için bize birden fazla istek göndermeyin. Bu web sitesi dışında olan mesajların hepsi cevapsız kalacaktır.",
+
+
+	/* ******************************************************************
+		====== COMMON ======
+	** ***************************************************************** */
+
+
+	/* ======================
+		@TABLES
+	** ====================== */
+	'TABLE_NAME'		=> "İsim",
+	'TABLE_REVIEWBY'	=> "İncelenme Tarihi",
+	'TABLE_SUPPORTEDV'	=> "Desteklenen Sürüm",
+	'TABLE_INFOS'		=> "Hakkında",
+
+
+	/* ======================
+		@ERROR
+	** ====================== */
+	'ERROR_PAGENOTFOUND'		=> "Bu sayfa artık mevcut değil",
+	'ERROR_PAGEACCESSDENIED'	=> "Bu sayfayı görütülemek için yetkin yok.",
+
+	'ERROR_REQUIRED_LOGIN'	=> "İşlemi gerçekleştirmek için giriş yapmalısın.",
+	'ERROR_INDEV'	=> "Site geliştirme aşamasındadır, lütfen sabırlı olun.",
+
+	'ERROR_PAGENOTFULL'	=> "Bu sayfa tamamlanmamıştır, bağlantılar tam olarak çalışmamaktadır!",
+	'ERROR_NOSERVERS'	=> "Sunucu bulunamadı. Sunucu kurmak ister misin?",
+
+	'ERROR_EMPTYFORMS'	=> "Bu form boş bırkılamaz.",
+	'ERROR_IDENTICALVALUES'	=> "Bilgiler aynı görünüyor.",
+
+
+	'ERROR_LOGIN_INVALIDCREDENTIALS'	=> "Bilgiler hatalı. <a href='".Url('cockpit/forgot/')."'>Şifrenizi mi unuttunuz?</a>",
+	
+	'ERROR_ACCOUNT_ACTIVATED'			=> "Hesabınız şu an aktif.",
+	
+	'ERROR_REGISTER_PASSWORDNOTMATCH'		=> "Şifreler eşleşmiyor.",
+	'ERROR_REGISTER_INVALIDFORMAT'			=> "Yanlış format",
+	'ERROR_REGISTER_USERNAMEALREADYEXIST'	=> "Bu isim zaten kullanılıyor.",
+	'ERROR_REGISTER_EMAILALREADYEXIST'		=> "Bu e-posta zaten kullanılıyor. <a href='".Url('cockpit/login/')."'>Bağlanmayı</a> dene.",
+	'ERROR_REGISTER_SUCCESS'				=> "Hesabınız yaratıldı. Lütfen mail adresinizi kontrol ediniz.",
+
+	'ERROR_INFOSETUP'	=> "Dokümanın tamamı <a class='text-danger' href='//wiki.h1emu.com/'>burada</a> bulunabilir.",
+	'ERROR_USELASTRELEASE' => "Şu anki proje sürümü ->",
+	
+	'ERROR_ACCOUNT_INFO_LEGACY'	=> "Hesap türü - <strong>Üyelik</strong>",
+	'ERROR_ACCOUNT_INFO_STEAM'	=> "Hesap türü - <strong>STEAM</strong>",
+
+	'ERROR_REGISTER_PASSWORDNOTGOOD'	=> "Şifre eşleşmiyor.",
+
+	'ERROR_RESETPASSWORD_MAILSENT'	=> "E-posta ile kodu gönderdik.",
+	'ERROR_RESETPASSWORD_INVALIDNEWPASS'	=> "Şifre eşleşmiyor.",
+	'ERROR_RESETPASSWORD_INVALIDCODE'	=> "Girilen kod hatalı.",
+
+	'ERROR_TABLE_EMPTY'	=> "Bilgi bulunamadı.",
+
+
+	/* ******************************************************************
+		INPUTS PLACEHOLDERS
+	** ***************************************************************** */
+	'INPUT_USERNAME'	=> "Kullanıcı Adı",
+	'INPUT_PASSWORD'	=> "Şifre",
+	'INPUT_PASSWORD_CONF'	=> "Şifre (tekrar yazınız)",
+	'INPUT_EMAIL'		=> "E-Posta",
+
+	'INPUT_ACTUALPASS'	=> "Kullandığınız Şifre",
+	'INPUT_NEWPASS'		=> "Yeni Şifre",
+	'INPUT_NEWPASSCONF'	=> "Yeni Şifre (tekrar yazınız)",
+	'INPUT_SERVERNAME'	=> "Sunucu İsmi",
+	'INPUT_FINALIZE'	=> "İsteği Tamamla",
+
+	'SLIDER_HOMETEXT'	=> "H1Z1 EMULATOR",
+
+	'INPUT_WIKIPAGENAME'	=> "Rehber sayfası ismi",
+
+	'INPUT_NEWSCONTENT'		=> "Haber içerği...",
+	'INPUT_RESPONSECONTENT'	=> "Cevabınızı buraya yazın...",
+	'INPUT_WIKICONTENT'		=> "Rehber sayfası içeriği...",
+
+	'INPUT_NEWPICTURE'	=> "Yükle",
+
+	/* ******************************************************************
+		CONSOLE
+	** ***************************************************************** */
+ 
+	// Sidenav Menu
+	'CONSOLE_HOME'		=> "Kontrol Paneli",
+	'CONSOLE_NEWS'		=> "Haberler",
+	'CONSOLE_SERVERS'	=> "Sunucu Listesi",
+	'CONSOLE_WIKI'		=> "Rehber Ayarları",
+	'CONSOLE_USERS'		=> "Kullanıcılar",
+	'CONSOLE_FINDUSER'	=> "Dosyayı Aç",
+	'CONSOLE_CONFIG'	=> "Site Ayarları",
+	'CONSOLE_REQUESTS'	=> "Sunucu İstekleri",
+	'CONSOLE_FORUMCATS'	=> "Forum Kategorileri",
+	'CONSOLE_FORUMMOD'	=> "Forum Yönetimi",
+
+	// Counters
+	'COUNTER_TOTAL_NEWS'	=> "Toplam Haberler",
+	'COUNTER_TOTAL_SERVERS'	=> "Toplam Sunucu",
+	'COUNTER_TOTAL_USERS'	=> "Toplam Kullanıcı",
+	'COUNTER_LASTUPDT'	=> "Son yayınlanma",
+
+	// Titles pages
+	'CONSOLE_TITLE_PAGE_HOME'	=> "Kontrol Paneli",
+	'CONSOLE_TITLE_PAGE_NEWS'	=> "Haberleri Yönet",
+	'CONSOLE_TITLE_PAGE_SERVERS'=> "Sunucuları Yönet",
+	'CONSOLE_TITLE_PAGE_USERS'	=> "Kullanıcıları Yönet",
+	'CONSOLE_TITLE_PAGE_CONFIG'	=> "Site Ayarları",
+	'CONSOLE_TITLE_PAGE_REQUESTS'	=> "Sunucu Listesi İstekleri",
+	'CONSOLE_TITLE_PAGE_WIKI'	=>  "Rehberi Yönet",
+	'CONSOLE_TITLE_PAGE_FORUMCAT'	=> "Forum Kategorilerini Yönet",
+
+	'CONSOLE_TITLE_PAGE_FOLDERVIEW'	=> "Oyuncu Dosyası",
+	'CONSOLE_TITLE_PAGE_FINDUSER'	=> "İstek Dosyası",
+	'CONSOLE_TITLE_PAGE_SUPPORTFOLDER_CODE'	=> "Doğrulama kodunu giriniz.",
+
+	'CONSOLE_TITLE_PAGE_EDITUSER'	=> "Kullanıcı düzenleniyor",
+	'CONSOLE_TITLE_PAGE_EDITNEWS'	=> "Haber düzenleniyor",
+	'CONSOLE_NEWS_ADD'	=> "Haber Ekle",
+
+	'SAVE'		=> "Kaydet",
+	'ENABLED'	=> "Etkin",
+	'DISABLED'	=> "Devre dışı",
+	/* ******************************************************************
+		DATE SYSTEM
+	** ***************************************************************** */
+	'DATE_SECOND'	=> "Saniye",
+	'DATE_MINUTES'	=> "Dakika",
+	'DATE_HOUR'		=> "Saat",
+	'DATE_DAYS'		=> "Gün",
+	'DATE_MONTH'	=> "Ay",
+	'DATE_YEAR'		=> "Yıl",
+
+
+	// Words
+	'EMULATOR' => "Emülatör",
+	'FAMILY' => "Ekip",
+	'COMMUNITY' => "Topluluk",
+
+	'YES'	=> "Evet",
+	'NO'	=> "Hayır",
+	'SUBMIT'	=> "Gönder",
+	'COPY'	=> "Kopyala",
+	'BACK2'	=> "Geri",
+	'BACK'	=> "Ana sayfaya geri dön",
+	'ERROR_GCODE'	=> "Oyun Hatası",
+	'UUID_RESET'	=> "UUID'yi Sıfırla",
+	'UUID_COPYED'	=> "Kopyalandı!",
+	'CREATED_AT'	=> "Oluşturuldu",
+	'IN'	=> "içeri",
+	// 'HOST_MSGE'	=> "Sunucu için daha fazla performansa mı ihtiyacınız var? Bu <a href=''>".($conf['hostperf']) ? $conf['hostperf']:"NoHostDefined"."</a> servisi kullanabilirsiniz.",
+
+];
+return $lang;
+?>
